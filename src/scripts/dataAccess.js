59f2084c5695fa6d1939646532ab76bc3821e66a
eschnei2 <everettschneider@gmail.com>
2021-06-16 15:40:16 -0500

    Finished

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -2,6 +2,7 @@ const applicationState = {
     requests: []
 }
 
+const mainContainer = document.querySelector("#container")
 const API = "http://localhost:8088"
 
 export const fetchRequests = () => {
@@ -17,4 +18,30 @@ export const fetchRequests = () => {
 
 export const getRequests = () => {
     return [...applicationState.requests]
+}
+
+export const sendRequest = (userServiceRequest) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userServiceRequest)
+    }
+
+
+    return fetch(`${API}/requests`, fetchOptions)
+        .then(response => response.json())
+        .then(() => {
+            mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+        })
+}
+
+export const deleteRequest = (id) => {
+    return fetch(`${API}/requests/${id}`, { method: "DELETE" })
+        .then(
+            () => {
+                mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+            }
+        )
 }
